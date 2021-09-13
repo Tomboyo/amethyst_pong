bd95ce1e5372de1b7ad4c6cab293f177da935dd5
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2021-09-12 21:40:27 -0400

    Use delta timing for paddles

--- src/systems/paddle.rs
+++ src/systems/paddle.rs
@@ -1,3 +1,4 @@
+use amethyst::core::timing::Time;
 use amethyst::core::Transform;
 use amethyst::derive::SystemDesc;
 use amethyst::ecs::{Join, Read, ReadStorage, System, SystemData, WriteStorage};
@@ -13,9 +14,10 @@ impl<'s> System<'s> for PaddleSystem {
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Paddle>,
         Read<'s, InputHandler<StringBindings>>,
+        Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, paddles, input): Self::SystemData) {
+    fn run(&mut self, (mut transforms, paddles, input, time): Self::SystemData) {
         for (paddle, transform) in (&paddles, &mut transforms).join() {
             match paddle.side {
                 Side::Left => input.axis_value("left_paddle"),
@@ -25,7 +27,7 @@ impl<'s> System<'s> for PaddleSystem {
                 // TODO: amethyst::core::timing::Time for delta time impl
                 let paddle_y = transform.translation().y;
                 transform.set_translation_y(
-                    (paddle_y + (1.2 * x))
+                    (paddle_y + (25.0 * x * time.delta_seconds()))
                         .min(ARENA_HEIGHT - (PADDLE_HEIGHT / 2.0))
                         .max(PADDLE_HEIGHT / 2.0),
                 );
